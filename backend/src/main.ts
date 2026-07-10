962005b706c6ca91b859f2f69a09371b7c3af2d0
Gayan Kuruppu <gayan.kuruppu@live.com>
2026-07-10 11:20:57 +0100

    chore: initialise

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true,
+  });
+
+  const port = Number(process.env.PORT ?? 3001);
+  await app.listen(port);
+}
+
+bootstrap();
